398697ce4f5b6dec2942d7e26ba90ce58b8e406d
Maz <ma.mazmaz@gmail.com>
2013-01-22 19:02:11 -0500

    added log viewer

--- admin_controller.py
+++ admin_controller.py
@@ -1,13 +1,24 @@
 import webapp2
 from handlers import AdminHandler
 import env
-from google.appengine.api import users
+from models import LogEntry
 
-class AdminIndex(AdminHandler):
+class IndexHandler(AdminHandler):
     def get(self):
         self.render_template("admin/index.html")
+class LogsHandler(AdminHandler):
+    PER_PAGE=20
+    def get(self):
+        page=int(self.request.get('page')) if self.request.get('page') else 0
+        offset=page*LogsHandler.PER_PAGE
+        def generate_ctx():
+            return LogEntry.all().order('-time')
+        entries=generate_ctx().run(offset=offset,limit=LogsHandler.PER_PAGE)
+        more_pages=generate_ctx().count(offset=offset+LogsHandler.PER_PAGE,limit=1)
+        self.render_template("admin/logs.html",entries=entries,page=page,more_pages=more_pages)
 
 app = webapp2.WSGIApplication([
-    ('/admin',AdminIndex),
-    ('/admin/',AdminIndex)
+    ('/admin',IndexHandler),
+    ('/admin/',IndexHandler),
+    ('/admin/logs',LogsHandler)
 ], debug=(env.env==env.DEVELOPMENT))
